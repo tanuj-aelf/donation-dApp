81e962ca969efeac8e08a9f6a39428636fb8b540
tanuj-aelf <tanuj.singh@aelf.io>
2025-01-11 17:23:31 +0530

    added donatorList unit test cases

--- test/DonationDAppTests.cs
+++ test/DonationDAppTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using AElf.Types;
+using AElf.Contracts.MultiToken;
 using Google.Protobuf.WellKnownTypes;
 using Shouldly;
 using Xunit;
@@ -305,6 +306,213 @@ namespace AElf.Contracts.DonationApp
 
             // Assert
             campaign.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task CreateCampaign_EmptyTitle_ShouldFail()
+        {
+            // Arrange
+            await DonationContract.Initialize.SendAsync(new InitializeInput());
+            
+            var input = new CreateCampaignInput
+            {
+                Title = "",  // Empty title
+                Description = "Test Description",
+                TargetAmount = 100_00000000,
+                StartTime = GetTimestamp(),
+                EndTime = GetTimestamp(30)
+            };
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<Exception>(() =>
+                DonationContract.CreateCampaign.SendAsync(input));
+            exception.Message.ShouldContain("Title cannot be empty");
+        }
+
+        [Fact]
+        public async Task CreateCampaign_ZeroTargetAmount_ShouldFail()
+        {
+            // Arrange
+            await DonationContract.Initialize.SendAsync(new InitializeInput());
+            
+            var input = new CreateCampaignInput
+            {
+                Title = "Test Campaign",
+                Description = "Test Description",
+                TargetAmount = 0,  // Zero target amount
+                StartTime = GetTimestamp(),
+                EndTime = GetTimestamp(30)
+            };
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<Exception>(() =>
+                DonationContract.CreateCampaign.SendAsync(input));
+            exception.Message.ShouldContain("Target amount must be greater than 0");
+        }
+
+        [Fact]
+        public async Task Donate_ZeroAmount_ShouldFail()
+        {
+            // Arrange
+            await DonationContract.Initialize.SendAsync(new InitializeInput());
+            
+            var createResult = await DonationContract.CreateCampaign.SendAsync(new CreateCampaignInput
+            {
+                Title = "Test Campaign",
+                Description = "Test Description",
+                TargetAmount = 100_00000000,
+                StartTime = GetTimestamp(),
+                EndTime = GetTimestamp(30)
+            });
+            var campaignId = createResult.Output.Value;
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<Exception>(() =>
+                DonationContract.Donate.SendAsync(new DonateInput
+                {
+                    CampaignId = campaignId,
+                    Amount = 0  // Zero donation amount
+                }));
+            exception.Message.ShouldContain("Donation amount must be greater than 0");
+        }
+
+        [Fact]
+        public async Task Donate_ExceedTargetAmount_ShouldFail()
+        {
+            // Arrange
+            await DonationContract.Initialize.SendAsync(new InitializeInput());
+            
+            var targetAmount = 10_00000000;  // 10 ELF
+            var createResult = await DonationContract.CreateCampaign.SendAsync(new CreateCampaignInput
+            {
+                Title = "Test Campaign",
+                Description = "Test Description",
+                TargetAmount = targetAmount,
+                StartTime = GetTimestamp(),
+                EndTime = GetTimestamp(30)
+            });
+            var campaignId = createResult.Output.Value;
+
+            // First donation equals target amount
+            await ApproveTokenAsync(DonationContractAddress, targetAmount);
+            await DonationContract.Donate.SendAsync(new DonateInput
+            {
+                CampaignId = campaignId,
+                Amount = targetAmount
+            });
+
+            // Try to donate more
+            await ApproveTokenAsync(DonationContractAddress, DefaultDonationAmount);
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<Exception>(() =>
+                DonationContract.Donate.SendAsync(new DonateInput
+                {
+                    CampaignId = campaignId,
+                    Amount = DefaultDonationAmount
+                }));
+            exception.Message.ShouldContain("Campaign target amount reached");
+        }
+
+        [Fact]
+        public async Task GetCampaign_NonExistentCampaign_ShouldReturnNull()
+        {
+            // Arrange
+            await DonationContract.Initialize.SendAsync(new InitializeInput());
+
+            // Act
+            var campaign = await DonationContract.GetCampaign.CallAsync(new StringValue { Value = "nonexistent" });
+
+            // Assert
+            campaign.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task GetDonatorList_EmptyCampaign_ShouldReturnEmptyList()
+        {
+            // Arrange
+            await DonationContract.Initialize.SendAsync(new InitializeInput());
+            
+            var createResult = await DonationContract.CreateCampaign.SendAsync(new CreateCampaignInput
+            {
+                Title = "Test Campaign",
+                Description = "Test Description",
+                TargetAmount = 100_00000000,
+                StartTime = GetTimestamp(),
+                EndTime = GetTimestamp(30)
+            });
+            var campaignId = createResult.Output.Value;
+
+            // Act
+            var donatorList = await DonationContract.GetDonatorList.CallAsync(new StringValue { Value = campaignId });
+
+            // Assert
+            donatorList.Value.Count.ShouldBe(0);
+        }
+
+        [Fact]
+        public async Task GetDonatorList_MultipleDonors_Success()
+        {
+            // Arrange
+            await DonationContract.Initialize.SendAsync(new InitializeInput());
+            
+            var createResult = await DonationContract.CreateCampaign.SendAsync(new CreateCampaignInput
+            {
+                Title = "Test Campaign",
+                Description = "Test Description",
+                TargetAmount = 100_00000000,
+                StartTime = GetTimestamp(),
+                EndTime = GetTimestamp(30)
+            });
+            var campaignId = createResult.Output.Value;
+
+            // First donor (default account)
+            await ApproveTokenAsync(DonationContractAddress, DefaultDonationAmount);
+            await DonationContract.Donate.SendAsync(new DonateInput
+            {
+                CampaignId = campaignId,
+                Amount = DefaultDonationAmount
+            });
+
+            // Second donor (User1)
+            var user1DonationContract = GetTester<DonationDAppContainer.DonationDAppStub>(DonationContractAddress, User1KeyPair);
+            var user1TokenContract = GetTester<TokenContractContainer.TokenContractStub>(TokenContractAddress, User1KeyPair);
+            
+            await user1TokenContract.Approve.SendAsync(new ApproveInput
+            {
+                Spender = DonationContractAddress,
+                Symbol = TokenSymbol,
+                Amount = DefaultDonationAmount
+            });
+
+            await user1DonationContract.Donate.SendAsync(new DonateInput
+            {
+                CampaignId = campaignId,
+                Amount = DefaultDonationAmount
+            });
+
+            // Act
+            var donatorList = await DonationContract.GetDonatorList.CallAsync(new StringValue { Value = campaignId });
+
+            // Assert
+            donatorList.Value.Count.ShouldBe(2);
+            donatorList.Value[0].Donor.ShouldBe(DefaultAddress);
+            donatorList.Value[0].Amount.ShouldBe(DefaultDonationAmount);
+            donatorList.Value[1].Donor.ShouldBe(User1Address);
+            donatorList.Value[1].Amount.ShouldBe(DefaultDonationAmount);
+        }
+
+        [Fact]
+        public async Task GetDonatorList_NonExistentCampaign_ShouldReturnEmptyList()
+        {
+            // Arrange
+            await DonationContract.Initialize.SendAsync(new InitializeInput());
+
+            // Act
+            var donatorList = await DonationContract.GetDonatorList.CallAsync(new StringValue { Value = "nonexistent" });
+
+            // Assert
+            donatorList.Value.Count.ShouldBe(0);
         }
     }
 } 
